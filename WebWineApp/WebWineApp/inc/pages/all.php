98eb17ec8ce1b14eb536fbac6868eab117941519
Agne <agne240@me.com>
2015-12-01 14:04:49 +0100

    share a wine

--- WebWineApp/WebWineApp/inc/pages/all.php
+++ WebWineApp/WebWineApp/inc/pages/all.php
@@ -1,7 +1,7 @@
 <?php
-$files = glob('inc/pages/*.{php}', GLOB_BRACE);
-foreach($files as $file) {
-  include_once($file);
-}
-
+    $files = glob('inc/pages/*.{php}', GLOB_BRACE);
+    foreach($files as $file) {
+        echo("<!-- ".$file." -->");
+        include_once($file);
+    }
 ?>
---
+++ WebWineApp/WebWineApp/inc/pages/share_wine.php
@@ -0,0 +1,41 @@
+<div class="container" date-page="page_share_wine">
+   <div class="heroContainer">
+        <div class="menu">
+            <i class="fa fa-bars fa-lg fa-fw color-white" id="menu"></i>
+        </div>
+        <div class="page-header">
+            <h1>Share Wine</h1>
+        </div>
+   </div>
+   <div class="actions">
+       <ul>
+            <li>
+                <div class="action-icon">
+                   <i class="fa fa-arrow-right fa-2x fa-fw"></i>
+                </div>
+                <div class="action">
+                   Title
+                   <small><input type="text" value=""></small>
+                </div>
+            </li>
+            <li class="row-30">
+                <div class="action-icon">
+                    <i class="fa fa-arrow-right fa-2x fa-fw"></i>
+                </div>
+               <div class="action">
+                   Title
+                   <small><input type="text" value=""></small>
+                </div>
+            </li>
+            <li class="row-10">
+                 <div class="action-icon">
+                    <i class="fa fa-arrow-right fa-2x fa-fw"></i>
+                </div>
+               <div class="action">
+                   Title
+                   <small><input type="text" value=""></small>
+                </div>
+            </li>
+       </ul>
+   </div>
+</div>
