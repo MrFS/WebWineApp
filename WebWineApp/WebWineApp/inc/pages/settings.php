02401ccd9d3c9c24d71ea8f119a133e2694acd2e
Agne <agne240@me.com>
2015-12-01 13:37:10 +0100

    php files added content

---
+++ WebWineApp/WebWineApp/inc/pages/settings.php
@@ -0,0 +1,45 @@
+<div class="container" date-page="page_settings">
+   <div class="heroContainer hero2">
+        <div class="menu">
+            <i class="fa fa-bars fa-lg fa-fw color-white" id="menu"></i>
+        </div>
+        <div class="page-header">
+            <h1><i class="fa fa-cog"></i> Settings</h1>
+        </div>
+   </div>
+   <div class="actions">
+       <ul>
+            <li>
+                <div class="action-icon">
+                   <i class="fa fa-share fa-2x fa-fw"></i>
+                </div>
+                <div class="action">
+                    Yes
+                    <small>Submit new wine for all your followers</small>
+                </div>
+            </li>
+            <li>
+                <div class="action-icon">
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-glass fa-2x fa-fw fa-stack-2x"></i>
+                        <i class="fa fa-star fa-lg fa-fw fa-stack-1x color-gold" style="margin-top: -22px;"></i>
+                    </span>
+
+                </div>
+                <div class="action">
+                    more  Wine
+                    <small>Submit new wine to your private collection</small>
+                </div>
+            </li>
+            <li>
+                <div class="action-icon">
+                   <i class="fa fa-glass fa-2x fa-fw"></i>
+                </div>
+                <div class="action">
+                    Browse Followers
+                    <small>Look what your friends like</small>
+                </div>
+            </li>
+       </ul>
+   </div>
+</div>
